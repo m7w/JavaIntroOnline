2c560c1eff715cc5bc3c3fbf24a0089c4788d763
Max Kavaliou <mkovaliou@gmail.com>
2021-05-24 00:14:51 +0300

    Added tasks 12 and 13 from decomposition.

--- jonline-mod02/src/main/java/com/github/m7w/mod02/_4decomp/Task412.java
+++ jonline-mod02/src/main/java/com/github/m7w/mod02/_4decomp/Task412.java
@@ -1,9 +1,71 @@
 package com.github.m7w.mod02._4decomp;
 
+import java.util.Scanner;
+
 /**
  * 12. Даны натуральные числа К и N. Написать метод(методы) формирования массива А, элементами которого
  * являются числа, сумма цифр которых равна К и которые не большее N.
  */
 public class Task412 {
-}
 
+    public static void main(String[] args) {
+
+        int K, N;
+        RArrayInt A = new RArrayInt();
+
+        K = inputFromConsole("Введите число K: ");
+        N = inputFromConsole("Введите число N: ");
+
+        for (int i = 1; i < N; i++) {
+            int ds = digitsSum(i);
+            if (ds == K)                 
+                A.append(i);
+        }
+
+        int[] array = A.toArray();
+
+        System.out.println("А: ");
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    public static int inputFromConsole(String message) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int i;
+        
+        System.out.print(message);
+        while(!sc.hasNextInt()) {
+            System.out.println("Введено неверное число. " + message);
+            sc.next();
+        }
+        i = sc.nextInt();
+
+        return i;
+    }
+
+    public static int digitsSum(int num) {
+
+        int sum = 0;
+
+        do {
+            sum += getTail(num);
+            num = getHead(num);
+        } while (num > 0);
+
+        return sum;
+    }
+
+    public static int getTail(int num) {
+
+        return num % 10;
+    }
+
+    public static int getHead(int num) {
+
+        return num / 10;
+    }
+
+}
---
+++ jonline-mod02/src/main/java/com/github/m7w/mod02/_4decomp/Task413.java
@@ -0,0 +1,54 @@
+package com.github.m7w.mod02._4decomp;
+
+import java.util.Scanner;
+
+/**
+ * 13. Два простых числа называются «близнецами», если они отличаются друг от друга на 2 (например, 41 и 43).
+ * Найти и напечатать все пары «близнецов» из отрезка [n,2n], где n - заданное натуральное число больше 2. Для
+ * решения задачи использовать декомпозицию.
+ */
+public class Task413 {
+
+    public static void main(String[] args) {
+
+        long n;
+
+        do {
+            n = inputFromConsole("Введите число n: ");
+            if (n < 3)
+                System.out.println("Число n должно быть больше 2");
+        } while (n < 3);
+
+        for (long i = n; i <= 2 * n; i++) {
+            if (isPrime(i))
+                if (isPrime(i + 2))
+                    System.out.println(i + " " + (i + 2));
+        }
+    }
+
+    public static long inputFromConsole(String message) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        long i;
+
+        System.out.print(message);
+        while(!sc.hasNextLong()) {
+            System.out.print("Введено неверное число. " + message);
+            sc.next();
+        }
+        i = sc.nextLong();
+
+        return i;
+    }
+
+    public static boolean isPrime(long n) {
+
+        for (long i = 3; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
